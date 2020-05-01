27246cb1959595d20a313f6fabe9301e4c2210d1
Morgan Matney <morgan.matney89@gmail.com>
2020-05-01 16:48:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,10 @@ class TicTacToe
     [2, 4, 6], #Diagonal
   ]
 
-  def display_board
-    puts @board
+  def display_board(@board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
   def input_to_index
